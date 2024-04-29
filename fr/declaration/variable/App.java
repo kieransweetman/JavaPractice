de02bb2cfe8dce91512e73593ed42b0974e9f231
kieransweetman <kikisweetman@gmail.com>
2024-04-29 14:18:52 +0200

    Add variable declarations in App.java

---
+++ fr/declaration/variable/App.java
@@ -0,0 +1,35 @@
+package fr.declaration.variable;
+
+public class App {
+    // byte
+    private static byte myByte = 10;
+
+    // short
+    private static short myShort = 500;
+
+    // int
+    private static int myInt = 10000;
+
+    // long
+    private static long myLong = 15000L;
+
+    // float
+    private static float myFloat = 20.0f;
+
+    // double
+    private static double myDouble = 30.0;
+
+    // boolean
+    private static boolean myBoolean = true;
+
+    // char
+    private static char myChar = 'A';
+
+    private static String randomString = "Hello World \n: are you kieran";
+
+    char myChar2 = 'B';
+    
+    public static void main(String[] args) {
+        System.out.println(randomString);
+    }
+}
